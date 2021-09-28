693dd9f333b67682ba7810135477f4ed37069e68
Ubuntu <ubuntu@ip-172-31-81-184.ec2.internal>
2021-09-28 17:37:37 +0000

    added 01._0.2.py

---
+++ 0.1_0.2.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+# Created by: Mikayla Barthelette
+# Created on: Sept 2021
+# This program adds 0.1 and 0.2
+
+
+def main():
+    # this function adds the numbers
+    
+    print("0.1 + 0.2 = {0}.".format(0.1 + 0.2))
+    print("\nDone.")
+
+if __name__ == "__main__":
+    main()
